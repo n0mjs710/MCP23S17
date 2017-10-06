be6738e8a12c421ad16eccbd98023441b4216144
Andy T. Evans <coolate@gmail.com>
2017-10-05 23:49:52 -0400

    Revert "Remove unneeded inverse on pinMode word"

--- MCP23S17/MCP23S17.cpp
+++ MCP23S17/MCP23S17.cpp
@@ -126,8 +126,8 @@ void MCP::pinMode(uint8_t pin, uint8_t mode) {  // Accept the pin # and I/O mode
 }
 
 void MCP::pinMode(unsigned int mode) {     // Accept the word…
-  wordWrite(IODIRA, mode);                // Call the the generic word writer with start register and the mode cache
-  _modeCache = mode;
+  wordWrite(IODIRA, ~mode);                // Call the the generic word writer with start register and the mode cache
+  _modeCache = ~mode;
 }
 
 // THE FOLLOWING WRITE FUNCTIONS ARE NEARLY IDENTICAL TO THE FIRST AND ARE NOT INDIVIDUALLY COMMENTED
